344271305496459270ecb90e2608b1f1a9676e53
mona-creator <astronaut1129@gmail.com>
2020-07-31 22:31:16 +0900

    コメント・いいね機能

--- app/controllers/company_comments_controller.rb
+++ app/controllers/company_comments_controller.rb
@@ -1,2 +1,24 @@
 class CompanyCommentsController < ApplicationController
+	def create
+     	company = Company.find(params[:company_id])
+        comment = current_user.company_comments.new(company_comment_params)
+		comment.company_id = company.id
+	    comment.save
+	    redirect_to company_path(company)
+	end
+	def destroy
+    @company_comment = CompanyComment.find(params[:company_id])
+    if @company_comment.user != current_user
+      redirect_to request.referer
+    end
+    @company_comment.destroy
+    redirect_to request.referer
+
+    end
+
+	private
+
+    def company_comment_params
+    params.require(:company_comment).permit(:comment)
+    end
 end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,14 @@
 class FavoritesController < ApplicationController
+def create
+            company = Company.find(params[:company_id])
+            favorite = current_user.favorites.new(company_id: company.id)
+            favorite.save
+            redirect_to request.referrer || root_url
+        end
+        def destroy
+            company = Company.find(params[:company_id])
+            favorite = current_user.favorites.find_by(company_id: company.id)
+            favorite.destroy
+            redirect_to request.referrer || root_url
+        end
 end
